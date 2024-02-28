52c2250290821cde3dd008c0aeaca64a4051a700
phthanh2000 <phucthanh.work@gmail.com>
2024-02-28 17:10:04 +0700

    2024-02-28: Config cors for BE

--- BE/dist/index.js
+++ BE/dist/index.js
@@ -4,12 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = require("body-parser");
 const routes_user_1 = __importDefault(require("./routes/routes_user"));
 const routes_createdb_1 = __importDefault(require("./routes/routes_createdb"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
-app.use((0, body_parser_1.json)());
+var cors = require('cors');
+app.use(cors());
 app.use('/users', routes_user_1.default);
 app.use('/db', routes_createdb_1.default);
 app.listen(PORT, () => {
--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import { json } from 'body-parser';
 import router_user from './routes/routes_user';
 import router_createdb from './routes/routes_createdb';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(json());
+var cors = require('cors');
+app.use(cors());
+
 app.use('/users', router_user);
 app.use('/db', router_createdb);
 
