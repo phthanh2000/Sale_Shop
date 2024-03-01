fb0daed4c45fbab6b233f59e2e2f5b766da67294
phthanh2000 <phucthanh.work@gmail.com>
2024-03-01 19:57:10 +0700

    2024-03-01: Change code service from Promise to await/async

--- FE/src/service/service_user.js
+++ FE/src/service/service_user.js
@@ -1,7 +1,7 @@
 import { Service_Base } from "./service_base";
 
 export class Service_User {
-    static GetAllUser = async () => {
+    static GetUser = async () => {
         const endpoint = '/users/getUsers';
         try {
             const data = await Service_Base.MethodGet(endpoint);
