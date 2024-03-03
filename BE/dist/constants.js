9d8272ebdeec00b076f7d6972aa05a58f9e3539c
phthanh2000 <phucthanh.work@gmail.com>
2024-03-03 11:52:10 +0700

    2024-03-03:Add constants table name

--- BE/dist/constants.js
+++ BE/dist/constants.js
@@ -1,10 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectPostgres = void 0;
-exports.connectPostgres = {
+exports.CONST_TABLE_NAME = exports.CONST_CONNECT_POSTGRES = void 0;
+exports.CONST_CONNECT_POSTGRES = {
     user: 'postgres',
     host: 'localhost',
     database: 'sale_shop',
     password: '123456',
     port: 5432,
+};
+exports.CONST_TABLE_NAME = {
+    categories: `public."Categories"`,
+    products: `public."Products"`,
+    users: `public."Users"`,
+    orders: `public."Orders"`,
+    orderDetails: `public."OrderDetails"`,
 };
--- BE/src/constants.ts
+++ BE/src/constants.ts
@@ -1,7 +1,15 @@
-export const connectPostgres = {
+export const CONST_CONNECT_POSTGRES = {
     user: 'postgres',
     host: 'localhost',
     database: 'sale_shop',
     password: '123456',
     port: 5432,
+}
+
+export const CONST_TABLE_NAME = {
+    categories: `public."Categories"`,
+    products: `public."Products"`,
+    users: `public."Users"`,
+    orders: `public."Orders"`,
+    orderDetails: `public."OrderDetails"`,
 }
