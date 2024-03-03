1e90971b19ee8fd64cb5322cc2fe12be3e0e5ebc
phthanh2000 <phucthanh.work@gmail.com>
2024-03-03 11:55:12 +0700

    2024-03-03:Update protocol

--- BE/src/routes/routes_createdb.ts
+++ BE/src/routes/routes_createdb.ts
@@ -3,8 +3,8 @@ import { Controller_CreateDB } from '../controllers/controller_createdb';
 
 const router_createdb = Router();
 
-router_createdb.get('/createDatabase', Controller_CreateDB.createDatabase);
-router_createdb.get('/createTable', Controller_CreateDB.createTable);
-router_createdb.get('/deleteTable', Controller_CreateDB.deleteTable);
+router_createdb.post('/createDatabase', Controller_CreateDB.createDatabase);
+router_createdb.post('/createTable', Controller_CreateDB.createTable);
+router_createdb.delete('/deleteTable', Controller_CreateDB.deleteTable);
 
 export default router_createdb;
