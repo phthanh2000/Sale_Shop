b04ac19b68a635911af2ed8707ab47c47c4d0a8e
phthanh2000 <phucthanh.work@gmail.com>
2024-03-06 14:08:00 +0700

    2024-03-06:Update value

--- BE/src/db/connection.ts
+++ BE/src/db/connection.ts
@@ -3,17 +3,17 @@ import { CONST_CONNECT_POSTGRES } from '../constants';
 
 // Config for create new database
 export const newPool = new Pool({
-    user: CONST_CONNECT_POSTGRES.user,
-    host: CONST_CONNECT_POSTGRES.host,
-    password: CONST_CONNECT_POSTGRES.password,
-    port: CONST_CONNECT_POSTGRES.port,
+  user: CONST_CONNECT_POSTGRES.user,
+  host: CONST_CONNECT_POSTGRES.host,
+  password: CONST_CONNECT_POSTGRES.password,
+  port: CONST_CONNECT_POSTGRES.port,
 });
-  
-  // Config for connect present database
+
+// Config for connect present database
 export const pool = new Pool({
-    user: CONST_CONNECT_POSTGRES.user,
-    host: CONST_CONNECT_POSTGRES.host,
-    database: CONST_CONNECT_POSTGRES.database,
-    password: CONST_CONNECT_POSTGRES.password,
-    port: CONST_CONNECT_POSTGRES.port,
+  user: CONST_CONNECT_POSTGRES.user,
+  host: CONST_CONNECT_POSTGRES.host,
+  database: CONST_CONNECT_POSTGRES.database,
+  password: CONST_CONNECT_POSTGRES.password,
+  port: CONST_CONNECT_POSTGRES.port,
 });
