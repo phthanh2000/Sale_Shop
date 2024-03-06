55b330d05233608de197f4f798bd98473f1462c9
phthanh2000 <phucthanh.work@gmail.com>
2024-03-06 15:48:05 +0700

    Create service send methods to Server

--- FE/src/service/service_base.js
+++ FE/src/service/service_base.js
@@ -2,60 +2,88 @@ import axios from 'axios';
 import { CONST_URL } from '../constants';
 
 export class Service_Base {
-    static MethodGet = async (endpoint) => {
+    // HTTP method get
+    static MethodGet = async (endpoint, token) => {
         const url = CONST_URL + endpoint;
-        try{
-            const response = await axios.get(url);
-            if(response.status === 200){
-                return response.data;
-            }
-        } catch (error){
-            if(error.request.status === 400){
-                throw error.response.data;
-            }
-            throw error.message; 
-        }
-    };
-
-    static MethodPost = async (endpoint, data) => {
-        const url = CONST_URL + endpoint;
-        try { 
-            const response = await Service_Base.MethodPost(url, data);
-            if(response.status === 200){
+        try {
+            const response = await axios.get(url,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+            if (response.status === 200) {
                 return response.data;
             }
         } catch (error) {
-            if(error.request.status === 400){
+            if (error.request.status === 400) {
                 throw error.response.data;
             }
-            throw error.message; 
+            throw error.message;
         }
     };
 
-    static MethodPut = async (endpoint, id, newData) => {
-        const url = CONST_URL + endpoint + `/${id}`;
-        try{
-            const response = await axios.put(url, newData);
-            if(response.status === 200){
+    // HTTP method post
+    static MethodPost = async (endpoint, token, data) => {
+        const url = CONST_URL + endpoint;
+        try {
+            const response = await axios.post(url, data,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+            if (response.status === 200) {
                 return response.data;
             }
-        } catch (error){
-            if(error.request.status === 400){
+        } catch (error) {
+            if (error.request.status === 400) {
                 throw error.response.data;
             }
-            throw error.message; 
+            throw error.message;
         }
     };
-    
-    static MethodDelete = async (endpoint, id) => {
-        const url = CONST_URL + endpoint + `/${id}`; 
+
+    // HTTP method put
+    static MethodPut = async (endpoint, token, id, newData) => {
+        const url = CONST_URL + endpoint + `/${id}`;
         try {
-            const response = await axios.delete(url);
-            if(response.status === 200){
+            const response = await axios.put(url, newData,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+            if (response.status === 200) {
                 return response.data;
             }
-        } catch (error){
-            if(error.request.status === 400){
+        } catch (error) {
+            if (error.request.status === 400) {
+                throw error.response.data;
+            }
+            throw error.message;
+        }
+    };
+
+    // HTTP method delete
+    static MethodDelete = async (endpoint, token, id) => {
+        const url = CONST_URL + endpoint + `/${id}`;
+        try {
+            const response = await axios.delete(url,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+            if (response.status === 200) {
+                return response.data;
+            }
+        } catch (error) {
+            if (error.request.status === 400) {
                 throw error.response.data;
             }
             throw error.message;
--- FE/src/service/service_user.js
+++ FE/src/service/service_user.js
@@ -1,23 +1,65 @@
+import axios from "axios";
 import { Service_Base } from "./service_base";
+import { CONST_URL } from "../constants";
 
 export class Service_User {
-    static GetUser = async () => {
+    // HTTP get list user
+    static GetUser = async (token) => {
         const endpoint = '/users/getUsers';
         try {
-            const data = await Service_Base.MethodGet(endpoint);
+            const data = await Service_Base.MethodGet(endpoint, token);
             return data;
         } catch (error) {
             throw error;
         }
     };
 
-    static DeleteUser = async (id) => {
-        const endpoint = '/users/deleteUser';
-        try{
-            const data = await Service_Base.MethodDelete(endpoint, id);
+    // HTTP create user (register)
+    static CreateUser = async (token, value) => {
+        const endpoint = '/users/createUser';
+        try {
+            const data = await Service_Base.MethodPost(endpoint, token, value);
             return data;
-        } catch (error){
+        } catch (error) {
             throw error;
+        }
+    }
+
+    // HTTP update user
+    static UpdateUser = async (token, id, newValue) => {
+        const endpoint = '/users/updateUser';
+        try {
+            const data = await Service_Base.MethodPut(endpoint, token, id, newValue);
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // HTTP delele user
+    static DeleteUser = async (token, id) => {
+        const endpoint = '/users/deleteUser';
+        try {
+            const data = await Service_Base.MethodDelete(endpoint, token, id);
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // HTTP login user
+    static UserLogin = async (data) => {
+        const endpoint = '/users/login';
+        try {
+            const url = CONST_URL + endpoint;
+            const response = await axios.post(url, data);
+            if (response.status === 200)
+                return response.data;
+        } catch (error) {
+            if (error.request.status === 400) {
+                throw error.response.data;
+            }
+            throw error.message;
         }
     }
 }
