f70b829895b17297f785fde9f7ee0b4edc343370
phthanh2000 <phucthanh.work@gmail.com>
2024-03-06 14:02:22 +0700

    2024-03-06:Add comment

--- BE/dist/index.js
+++ BE/dist/index.js
@@ -8,12 +8,19 @@ const body_parser_1 = require("body-parser");
 const cors_1 = __importDefault(require("cors"));
 const routes_user_1 = __importDefault(require("./routes/routes_user"));
 const routes_createdb_1 = __importDefault(require("./routes/routes_createdb"));
+// Using express
 const app = (0, express_1.default)();
+// Port is used
 const PORT = process.env.PORT || 3001;
+// Middleware for parse and process json data from client
 app.use((0, body_parser_1.json)());
+// Middleware for allow access from other domains
 app.use((0, cors_1.default)());
+// Middleware for route routing
 app.use('/users', routes_user_1.default);
 app.use('/db', routes_createdb_1.default);
+// Start an HTTP server and start listening for incoming requests from the specified port
+// app.listen(port, [callback]);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- BE/src/index.ts
+++ BE/src/index.ts
@@ -4,15 +4,23 @@ import cors from 'cors';
 import router_user from './routes/routes_user';
 import router_createdb from './routes/routes_createdb';
 
+// Using express
 const app = express();
+
+// Port is used
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parse and process json data from client
 app.use(json());
+// Middleware for allow access from other domains
 app.use(cors());
 
+// Middleware for route routing
 app.use('/users', router_user);
 app.use('/db', router_createdb);
 
+// Start an HTTP server and start listening for incoming requests from the specified port
+// app.listen(port, [callback]);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
