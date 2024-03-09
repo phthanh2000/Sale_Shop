ea57110634972b4315fa12e686320c264c02276c
phthanh2000 <phucthanh.work@gmail.com>
2024-03-09 23:24:11 +0700

    2024-03-09:Change protocol

--- BE/dist/routes/routes_databases.js
+++ BE/dist/routes/routes_databases.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const controller_databases_1 = require("../controllers/controller_databases");
 // Router create database
 const router_database = (0, express_1.Router)();
-router_database.post('/createDatabase', controller_databases_1.Controller_Databases.createDatabase);
-router_database.post('/createTable', controller_databases_1.Controller_Databases.createTable);
+router_database.get('/createDatabase', controller_databases_1.Controller_Databases.createDatabase);
+router_database.get('/createTable', controller_databases_1.Controller_Databases.createTable);
 router_database.delete('/deleteTable', controller_databases_1.Controller_Databases.deleteTable);
 exports.default = router_database;
--- BE/src/routes/routes_databases.ts
+++ BE/src/routes/routes_databases.ts
@@ -4,8 +4,8 @@ import { Controller_Databases } from '../controllers/controller_databases';
 // Router create database
 const router_database = Router();
 
-router_database.post('/createDatabase', Controller_Databases.createDatabase);
-router_database.post('/createTable', Controller_Databases.createTable);
+router_database.get('/createDatabase', Controller_Databases.createDatabase);
+router_database.get('/createTable', Controller_Databases.createTable);
 router_database.delete('/deleteTable', Controller_Databases.deleteTable);
 
 export default router_database;
