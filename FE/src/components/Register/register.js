ca8b3341347c91f237147b1c36d371d5ce87de88
phthanh2000 <phucthanh.work@gmail.com>
2024-03-11 13:13:47 +0700

    2024-03-11: Call API create user

--- FE/src/components/Register/register.js
+++ FE/src/components/Register/register.js
@@ -3,6 +3,7 @@ import { IoClose, IoEye, IoEyeOff } from "react-icons/io5";
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
 import './register.css';
+import { Service_User } from "../../service/service_user";
 
 export const Register = (props) => {
     // Value from name input
@@ -56,26 +57,55 @@ export const Register = (props) => {
     }
 
     // Event on click register button on register form
-    const onClickRegisterButton = () => {
-        if (name=== '') {
+    const onClickRegisterButton = async () => {
+        setReEnterPasswordMessage('');
+        if (name === '') {
             setNameMessage('Tên người dùng không được để trống');
-        } 
+        }
         if (email === '') {
             setEmailMessage('Email không được để trống');
-        } 
-        if (phone=== '') {
-            setPhoneMessage('Số điện không được để trống');
-        } 
+        }
+        // Check if the string has @ with Regular Expression
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            setEmailMessage('Email không đúng định dạng example@example.com');
+        }
+        if (phone === '') {
+            setPhoneMessage('Số điện thoại không được để trống');
+        }
+        // Check for a string with 10 digits with regular expression
+        const phoneRegex = /^\d{10}$/;
+        if (!phoneRegex.test(phone)) {
+            setPhoneMessage('Số điện thoại không đúng định dạng là số và có 10 chữ số');
+        }
         if (password === '') {
             setPasswordMessage('Mật khẩu không được để trống');
-        } 
-        if (reEnterPassword === ''){
+        }
+        if (reEnterPassword === '') {
             setReEnterPasswordMessage('Nhập lại mật khẩu không được đê trống')
         }
-        if (password !== '' && reEnterPassword !== '' && password !== reEnterPassword){
+        if (password !== '' && reEnterPassword !== '' && password !== reEnterPassword) {
             setReEnterPasswordMessage('Mật khẩu không khớp');
-        } else {
-            // Login API
+        }
+        if (name !== '' && email !== '' && emailRegex.test(email) && phone !== '' && phoneRegex.test(phone) && password !== '' && reEnterPassword !== '') {
+            // Login API create user
+            try {
+                const dataRegister = {
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    pass: password,
+                    roleid: 2
+                };
+                const result = await Service_User.CreateUser('', dataRegister);
+                if (result === 'Email is used') {
+                    setEmailMessage('Email đã được đăng ký');
+                } else {
+                    props.closeRegisterForm(!props.showRegisterForm, 'close');
+                }
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 
@@ -109,6 +139,7 @@ export const Register = (props) => {
                         <input type="text"
                             name="name"
                             placeholder="Nhập Tên người dùng"
+                            maxLength={100}
                             value={name}
                             onChange={(e) => setName(e.target.value)}
                             onClick={() => setNameMessage('')} />
@@ -123,6 +154,7 @@ export const Register = (props) => {
                         <input type="email"
                             name="email"
                             placeholder="Nhập Email"
+                            maxLength={100}
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             onClick={() => setEmailMessage('')} />
@@ -137,6 +169,7 @@ export const Register = (props) => {
                         <input type="tel"
                             name="phone"
                             placeholder="Nhập Số điện thoại"
+                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                             value={phone}
                             onChange={(e) => setPhone(e.target.value)}
                             onClick={() => setPhoneMessage('')} />
@@ -148,24 +181,16 @@ export const Register = (props) => {
                         Mật khẩu:
                     </label>
                     <div className="input-password">
+                        <input type={isShowPassword ? 'text' : 'password'}
+                            name="pass"
+                            placeholder="Nhập Mật khẩu"
+                            maxLength={50}
+                            onChange={(e) => setPassword(e.target.value)}
+                            onClick={() => setPasswordMessage('')} />
                         {isShowPassword ?
-                            <>
-                                <input type="text"
-                                    name="pass"
-                                    placeholder="Nhập Mật khẩu"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    onClick={() => setPasswordMessage('')} />
-                                <IoEye onClick={(e) => onClickIconEyeToPasswordDislay('pass')}></IoEye>
-                            </>
+                            <IoEye onClick={(e) => onClickIconEyeToPasswordDislay('pass')}></IoEye>
                             :
-                            <>
-                                <input type="password"
-                                    name="pass"
-                                    placeholder="Nhập Mật khẩu"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    onClick={() => setPasswordMessage('')} />
-                                <IoEyeOff onClick={(e) => onClickIconEyeToPasswordDislay('pass')}></IoEyeOff>
-                            </>
+                            <IoEyeOff onClick={(e) => onClickIconEyeToPasswordDislay('pass')}></IoEyeOff>
                         }
                     </div>
                     <div className={password === '' ? "message-notification show" : "message-notification hide"}>
@@ -175,24 +200,15 @@ export const Register = (props) => {
                         Nhập lại mật khẩu:
                     </label>
                     <div className="input-re-enter-password">
+                        <input type={isShowRePassword ? 'text' : 'password'}
+                            name="re-enter-password"
+                            placeholder="Nhập lại mật khẩu"
+                            onChange={(e) => setReEnterPassword(e.target.value)}
+                            onClick={() => setReEnterPasswordMessage('')} />
                         {isShowRePassword ?
-                            <>
-                                <input type="text"
-                                    name="re-enter-password"
-                                    placeholder="Nhập lại mật khẩu"
-                                    onChange={(e) => setReEnterPassword(e.target.value)}
-                                    onClick={() => setReEnterPasswordMessage('')} />
-                                <IoEye onClick={(e) => onClickIconEyeToPasswordDislay('re-pass')}></IoEye>
-                            </>
+                            <IoEye onClick={(e) => onClickIconEyeToPasswordDislay('re-pass')}></IoEye>
                             :
-                            <>
-                                <input type="password"
-                                    name="re-enter-password"
-                                    placeholder="Nhập lại mật khẩu"
-                                    onChange={(e) => setReEnterPassword(e.target.value)}
-                                    onClick={() => setReEnterPasswordMessage('')} />
-                                <IoEyeOff onClick={(e) => onClickIconEyeToPasswordDislay('re-pass')}></IoEyeOff>
-                            </>
+                            <IoEyeOff onClick={(e) => onClickIconEyeToPasswordDislay('re-pass')}></IoEyeOff>
                         }
                     </div>
                     <div className={reEnterPassword === '' ? "message-notification show" : "message-notification hide"}>
