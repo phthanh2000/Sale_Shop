603cae01596e92451ef1d851e8256c7bb4ac5637
phthanh2000 <phucthanh.work@gmail.com>
2024-06-17 17:54:40 +0700

    2024-06-17: Add route for update password user

--- BE/dist/routes/routes_users.js
+++ BE/dist/routes/routes_users.js
@@ -10,6 +10,7 @@ router_user.post('/getUserInfo', controller_users_1.Controller_Users.getUserInfo
 router_user.post('/login', controller_users_1.Controller_Users.userLogin);
 router_user.post('/registerUser', controller_users_1.Controller_Users.createUser);
 router_user.put('/editUser/:id', controller_base_1.Controller_Base.authenticateToken, controller_users_1.Controller_Users.updateUser);
+router_user.put('/editPasswordUser/:id', controller_base_1.Controller_Base.authenticateToken, controller_users_1.Controller_Users.updatePassswordUser);
 router_user.delete('/deleteUser/:id', controller_base_1.Controller_Base.authenticateToken, controller_users_1.Controller_Users.deleteUser);
 router_user.post('/forgetPasswordUser', controller_users_1.Controller_Users.forgetPasswordUser);
 router_user.post('/checkTokenExpired', controller_users_1.Controller_Users.checkTokenExpired);
--- BE/src/routes/routes_users.ts
+++ BE/src/routes/routes_users.ts
@@ -10,6 +10,7 @@ router_user.post('/getUserInfo', Controller_Users.getUserInfo);
 router_user.post('/login', Controller_Users.userLogin);
 router_user.post('/registerUser', Controller_Users.createUser);
 router_user.put('/editUser/:id', Controller_Base.authenticateToken, Controller_Users.updateUser);
+router_user.put('/editPasswordUser/:id', Controller_Base.authenticateToken, Controller_Users.updatePassswordUser);
 router_user.delete('/deleteUser/:id', Controller_Base.authenticateToken, Controller_Users.deleteUser);
 router_user.post('/forgetPasswordUser', Controller_Users.forgetPasswordUser);
 router_user.post('/checkTokenExpired', Controller_Users.checkTokenExpired);
